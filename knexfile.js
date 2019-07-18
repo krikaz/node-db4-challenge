8102981125babce1ed204f623792cb04ff3db6a3
krikaz <pascal.bou@protonmail.com>
2019-07-18 14:15:06 +0200

    added pragma

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,11 @@ module.exports = {
 		},
 		useNullAsDefault: true,
 	},
+	pool: {
+		afterCreate: (conn, done) => {
+			conn.run('PRAGMA foreign_keys = ON', done);
+		},
+	},
 	migrations: {
 		directory: './data/migrations',
 	},
