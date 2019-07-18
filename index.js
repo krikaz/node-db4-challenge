f88d38d9d23153261ab46c9b6058ed9f3256f713
krikaz <pascal.bou@protonmail.com>
2019-07-18 15:19:38 +0200

    corrected getShoppingList

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const app = express();
 app.use(express.json());
 
 function getRecipes() {
-	return db
-		.select('recipes.name')
-		.from('recipes')
+	return db.select('recipes.name').from('recipes');
 }
 
 function getShoppingList(recipe_id) {
 	return db
-		.select('recipes.name', 'quantities.quantity', 'ingredients.name')
-		.from('recipes')
-		.innerJoin('quantities')
+		.select('quantities.quantity', 'ingredients.name')
+		.from('steps')
+		.innerJoin('quantities', 'quantities.id', 'steps.quantity_id')
+		.innerJoin('ingredients', 'ingredients.id', 'steps.ingredient_id')
+		.where('steps.recipe_id', recipe_id);
 }
 
 app.listen(4000, () => {
